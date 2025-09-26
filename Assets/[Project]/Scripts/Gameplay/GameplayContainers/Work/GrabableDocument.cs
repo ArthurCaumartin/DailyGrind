55693f5b26cd8fe0735c1e9e2643803d0a17966a
Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
2025-09-26 16:31:43 +0200

    background transition animation

--- Assets/[Project]/Scripts/Gameplay/GameplayContainers/Work/GrabableDocument.cs
+++ Assets/[Project]/Scripts/Gameplay/GameplayContainers/Work/GrabableDocument.cs
@@ -24,8 +24,11 @@ public class GrabableDocument : Grabable
 
     protected Storage TryGetStorage()
     {
+        if (this == null || gameObject == null)
+            return null;
+            
         Collider2D col = Physics2D.OverlapBox(transform.position, Vector2.one, 0, LayerMask.GetMask("Storage"));
-        if(!col) return null;
+        if (!col) return null;
         Storage storage = col.GetComponent<Storage>();
         return storage;
     }
--- Assets/[Project]/Scripts/Visual/VisualSetter.cs
+++ Assets/[Project]/Scripts/Visual/VisualSetter.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class VisualSetter : MonoBehaviour
@@ -5,19 +6,53 @@ public class VisualSetter : MonoBehaviour
     [SerializeField] private SpriteRenderer _playerSpriteRenderer;
     [SerializeField] private SpriteRenderer _backgroundSpriteRenderer;
     [SerializeField] private SpriteRenderer _deskSpriteRenderer;
+    [SerializeField] private SpriteRenderer _transitionSpriteRenderer;
+
+    private void Start()
+    {
+        Vector3 botLeft = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0));
+        botLeft.z = 0;
+        _backgroundSpriteRenderer.transform.position = botLeft;
+        _transitionSpriteRenderer.sprite = null;
+    }
 
     public void SetPlayer(Sprite sprite)
     {
+        if (!sprite) return;
         _playerSpriteRenderer.sprite = sprite;
     }
 
     public void SetBackground(Sprite sprite)
     {
-        _backgroundSpriteRenderer.sprite = sprite;
+        if (!sprite) return;
+        BackAnimation(sprite);
     }
 
     public void SetDesk(Sprite sprite)
     {
+        if (!sprite) return;
         _deskSpriteRenderer.sprite = sprite;
+    }
+
+    private void BackAnimation(Sprite sprite)
+    {
+        Vector3 botLeft = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0));
+        botLeft.z = 0;
+
+        Vector3 botRight = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth, 0, 0));
+        botRight.z = 0;
+
+        _transitionSpriteRenderer.transform.position = botRight;
+        _transitionSpriteRenderer.sprite = sprite;
+        DOTween.To((time) =>
+        {
+            Vector3 pos = Vector3.Lerp(botRight, botLeft, time);
+            _transitionSpriteRenderer.transform.position = pos;
+        }, 0, 1, 1)
+        .OnComplete(() =>
+        {
+            _backgroundSpriteRenderer.sprite = sprite;
+            _transitionSpriteRenderer.transform.position = botRight;
+        });
     }
 }
